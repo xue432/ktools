9bb34e7cd4e625d4d377100a6c2fcbd1e902a117
kalvinGit <linkaihui2012@sina.com>
2018-11-05 17:50:15 +0800

    首页优化

--- src/main/resources/static/js/ktPublic.js
+++ src/main/resources/static/js/ktPublic.js
@@ -3,14 +3,6 @@
  * @type {index|BigInt|number|*|jQuery}
  */
 
-// 导航选中
-var itemEle = $('.navbar-nav').find('.nav-link');
-itemEle.on('click', function (event) {
-    var src = $(event.currentTarget).attr('href');
-    var txt = $(event.currentTarget).text();
-    log(txt)
-});
-
 // 页签选择更新面包悄事件
 $('.nav-tabs').find('.nav-link').on('click', function (event) {
     $('.breadcrumb').find('.active').text($(event.currentTarget).text());
@@ -21,4 +13,17 @@ $('.kt-img-box').hover(function(event) {
     $(event.currentTarget).find('.kt-img-inf').animate({"margin-top": "-20px"}, 100, "swing");
 },function(event) {
     $(event.currentTarget).find('.kt-img-inf').stop(true, false).animate({"margin-top": "0"}, 100, "swing");
+});
+
+// 鼠标经过导航栏下拉导航展开效果
+var dropdownEle = $('.dropdown');
+log(dropdownEle);
+dropdownEle.on('mouseover', function (event) {
+    log(event.currentTarget);
+    $(event.currentTarget).addClass('show');
+});
+
+// 鼠标离开导航栏下拉导航收起效果
+dropdownEle.on('mouseleave', function (event) {
+    $(event.currentTarget).removeClass('show');
 });
